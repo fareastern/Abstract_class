32cba1cb12f0fe6a1fcbb068ed35ceed50de83fc
fareastern <36926187+fareastern@users.noreply.github.com>
2025-03-21 17:45:22 +0300

    Add files via upload

---
+++ CreditAccount.java
@@ -0,0 +1,25 @@
+class CreditAccount extends Account {
+    private final long creditLimit;
+
+    public CreditAccount(long creditLimit) {
+        this.creditLimit = creditLimit;
+    }
+
+    @Override
+    public boolean add(long amount) {
+        if (balance + amount <= 0) {
+            balance += amount;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean pay(long amount) {
+        if (balance - amount >= creditLimit) {
+            balance -= amount;
+            return true;
+        }
+        return false;
+    }
+}
